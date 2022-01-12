102039d4b0bdae13ef7e2c237acfa42ec9c356c1
vdegenne <vdegenne@gmail.com>
2022-01-12 15:19:31 +0100

    materiral

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { LitElement, html, css } from 'lit'
 import { customElement } from 'lit/decorators.js'
+import '@material/mwc-snackbar'
+// import '@material/mwc-button'
+// import '@material/mwc-icon-button'
+// import '@material/mwc-dialog'
+// import '@material/mwc-textfield'
+// import '@material/mwc-checkbox'
 
 declare global {
   interface Window {
