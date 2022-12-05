dcfc86a28f04570418b90aee35f623008c5e0567
Valentin Degenne <vdegenne@gmail.com>
2022-12-05 12:51:54 +0100

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -6,5 +6,6 @@ tmux new -d -s $session 'npm run watch'
 #tmux send-keys -t 'watch' 'npm run watch' C-m
 tmux split-window -h 'npm run browser-sync'
 #tmux neww -n 'browser-sync' 'npm run browser-sync'
+tmux split-window -f
 
 tmux attach-session -t $session:0
