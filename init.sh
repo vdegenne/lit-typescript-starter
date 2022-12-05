6065ec7dfd04b7e609e69dfb80af1174af0bfeb4
vdegenne <vdegenne@gmail.com>
2022-12-05 13:13:39 +0100

    init script

--- init.sh
+++ init.sh
@@ -3,7 +3,10 @@
 set -e
 
 rm -rf .git
+rm -f readme.md
 read -p 'Project name: ' projectname
 read -p 'port: ' port
+find . -type f -exec sed -i "s/%appname%/$(projectname)/g" {} +
+find . -type f -exec sed -i "s/%port%/$(port)/g" {} +
 
-rm readme.md
+rm -f init.sh
