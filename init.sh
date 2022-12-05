2d0dc6a17e9b0f5e0a8a04374b4e536e11273392
Valentin Degenne <vdegenne@gmail.com>
2022-12-05 13:21:25 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,9 +4,14 @@ set -e
 
 rm -rf .git
 rm -f readme.md
+
 read -p 'Project name: ' projectname
-read -p 'port: ' port
+read -p 'Port: ' port
 find . -type f -exec sed -i "s/%appname%/$projectname/g" {} +
 find . -type f -exec sed -i "s/%port%/$port/g" {} +
 
+code .
+npm i
+
 rm -f init.sh
+./tmux.sh
