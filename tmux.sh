d80ae1e1a9367e3271f57d3e72102614206216b8
vdegenne <vdegenne@gmail.com>
2022-12-05 13:46:13 +0100

    tmux

--- tmux.sh
+++ tmux.sh
@@ -4,8 +4,8 @@ session='%appname%'
 
 tmux new -d -s $session 'npm run watch'
 
-tmux split-window -h 'npm run browser-sync'
-#tmux split-window -h 'npm run browser-sync:static'
+tmux split-window -h 'npm run browser-sync:static'
+#tmux split-window -h 'npm run browser-sync'
 
 tmux split-window -f
 #tmux split-window -f 'pm2 start pm2.config.cjs'
